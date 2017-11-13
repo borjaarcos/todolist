88528e784a75491376758fbdecb5c3e5730d7913
borjaarcos <32301583+borjaarcos@users.noreply.github.com>
2017-11-13 22:13:16 +0100

    Create añadir.php

---
+++ añadir.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+require_once 'index.php';
+
+
+if($_POST && !empty($_POST['nombre'])) {
+  $name =htmlspecialchars($_POST['nombre']);}
+
+  if(!empty($name)){
+      $tareanuevasql="INSERT INTO tareas VALUES('', '".$name."', '".$_SESSION['id_usuario']."', '2000-10-10', '')";
+    $resultado2= $conn->query($tareanuevasql);
+}
+
+header('Location: tareas.php');
+?>
