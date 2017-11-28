6c06fde3fb51eff313e43a007bbbc5ebb34f4177
borjaarcos <32301583+borjaarcos@users.noreply.github.com>
2017-11-28 15:59:40 +0100

    Update borrar.php

--- borrar.php
+++ borrar.php
@@ -1,10 +1,11 @@
 <?php
 require_once 'tareas.php';
+include 'init.php';
       $row  = $_GET['id'];
 
 
       $consultaHecho = "DELETE FROM tareas
-                            WHERE id = '$row' AND usuario = 1";
+                            WHERE id = '$row' AND usuario = '".$_SESSION['id_usuario']."'";
 
         $resultado3= $conn->query($consultaHecho);
         var_dump($conn->error_list);
