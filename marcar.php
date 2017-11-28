bf31c6865fcbecc2b0c6ca9430941f92166972c2
borjaarcos <32301583+borjaarcos@users.noreply.github.com>
2017-11-28 16:01:32 +0100

    Update marcar.php

--- marcar.php
+++ marcar.php
@@ -1,14 +1,15 @@
 <?php
 require_once 'tareas.php';
+include 'init.php';
       $row  = $_GET['id'];
 
 
       $consultaHecho = "UPDATE tareas
                             SET hecho = 1
-                            WHERE id = '$row' AND usuario = 1";
+                            WHERE id = '$row' AND usuario = '".$_SESSION['id_usuario']."'";
 
         $resultado3= $conn->query($consultaHecho);
-        
+
 header('Location: tareas.php');
 
  ?>
